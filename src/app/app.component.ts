6b599a0514d8b3b56247354e491becce373f2a75
kevin4lexander <kevinamh24@gmail.com>
2018-06-12 17:23:17 -0500

    add persono feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Person } from './person.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  kevin: Person;
+
+  constructor() {
+    this.kevin = new Person('1234', 'Kevin Moreno', 24);
+  }
 }
---
+++ src/app/person.model.ts
@@ -0,0 +1,3 @@
+export class Person {
+    constructor(public id: string, public fullName: string, public age: number) {}
+}
